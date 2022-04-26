0aa9b51f9debac594b277ba113124b68de34742b
DamianRumak1 <100698199+DamianRumak@users.noreply.github.com>
2022-04-26 13:11:06 +0000

    wiek,data

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import java.io.IOException;
 
 class WrongStudentName extends Exception { }
+class WrongStudentAge extends Exception { }
+class WrongStudentDate extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -21,6 +23,12 @@ class Main {
 
             } catch(WrongStudentName e) {
                 System.out.println("Błędne imie studenta!");
+            }
+              catch(WrongStudentAge e) {
+                System.out.println("Błędny wiek!");
+            }
+            catch(WrongStudentDate e) {
+                System.out.println("Błędna data!");
             }
         }
     }
@@ -44,16 +52,32 @@ class Main {
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName {
-        var name = ReadName();
+    public static int ReadAge() throws WrongStudentAge {
         System.out.println("Podaj wiek: ");
         var age = scan.nextInt();
         scan.nextLine();
+      if ((age<0 || age>100))
+        throw new WrongStudentAge();
+      return age;
+      }
+  
+    public static String ReadDate() throws WrongStudentDate{
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
-        var date = scan.nextLine();
-        (new Service1()).addStudent(new Student(name, age, date));
+      var date = scan.nextLine();
+      if (date.length()>10|date.charAt(2)!='-'||date.charAt(5)!='-')
+        throw new WrongStudentDate();
+      return date;
     }
-
+  
+    public static void exercise1() throws IOException ,
+      WrongStudentName,WrongStudentAge,WrongStudentDate{
+        var name = ReadName();
+        var age = ReadAge();
+        var date = ReadDate();
+        (new Service1()).addStudent(new Student(name, age, date));
+        
+      }
+      
     public static void exercise2() throws IOException {
         var students = (new Service1()).getStudents();
         for(Student current : students) {
